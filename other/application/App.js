9ef26e06f088d30f0d92d120e5cd75589f1699fb
xhyrom <generalkubo@gmail.com>
2021-11-29 19:33:45 +0100

    app source

---
+++ other/application/App.js
@@ -0,0 +1,12 @@
+import React, { Component, useState } from 'react';
+import { WebView } from 'react-native-webview';
+
+export default class Application extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <WebView source={{ uri: "https://covid19.hyrousek.tk" }}></WebView>
+  }
+}
